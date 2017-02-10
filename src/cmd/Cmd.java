ffb4ede744e7912ea92a597b81c41f4657ea4c6a
lmameri <lycia.mameri@gmail.com>
2017-02-09 19:22:09 -0500

    commit 1

---
+++ src/cmd/Cmd.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cmd;
+
+/**
+ *
+ * @author ad091192
+ */
+public class Cmd {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Version 1
+    }
+    
+}
